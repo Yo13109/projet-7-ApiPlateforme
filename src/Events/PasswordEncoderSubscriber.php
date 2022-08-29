c03efb44fc5f9c85c0e37db5030d9c536ae0eb32
Yoann Corsi <yoann.corsi@gmail.com>
2022-08-29 14:54:36 +0200

    hash password

---
+++ src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Reseller;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface
+
+{
+    private $passwordHasher;
+
+        public function __construct(UserPasswordHasherInterface $passwordHasher)
+        {
+            $this->passwordHasher = $passwordHasher;
+        }
+
+    public static function getSubscribedEvents()
+    {
+        return[
+            KernelEvents::VIEW => ['encode password', EventPriorities::PRE_WRITE]
+        ];
+    }
+    
+    public function encodePassword(ViewEvent $event)
+    
+    {
+$result = $event->getControllerResult();
+$method = $event->getRequest()->getMethod();
+
+        if ($result instanceof Reseller && $method === "POST") {
+            $hash = $this->passwordHasher->hashPassword(
+                $result, $result->getPassword());
+            $result->setPassword($hash);
+        }
+
+    }
+    
+
+
+}
+
